91e1b9369243f60c42680093e637d0c06c987ce0
Vitali Sazonenka <vitali.sazonenka@pandadoc.com>
2025-02-24 21:38:13 +0100

    init: initial config

---
+++ lua/plugins/formatting.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
+        -- markdown = { "write_good", "mdformat" },
+        markdown = { "write_good", "markdownlint-cli2" },
+        proto = { "buf" },
+        json = { "jq" },
+        toml = { "taplo" },
+
+        ["*"] = { "trim_newlines", "trim_whitespace" },
+      },
+      formatters = {
+        goimports = {
+          prepend_args = { "-local", "gitlab.pandadoc.com" },
+        },
+        hclfmt = {
+          command = "hclfmt",
+        },
+        terragrunt_fmt = {
+          command = "terragrunt",
+          args = { "fmt", "-" },
+        },
+        taplo = {
+          args = { "format", "-c", vim.fn.expand("~/.taplo.toml"), "-" },
+        },
+        mdformat = {
+          command = "mdformat",
+          args = { "--wrap", "80", "-" },
+        },
+      },
+    },
+  },
+}
